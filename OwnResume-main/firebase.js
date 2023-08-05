e4156631dbc84da83303e5808fc1c20badd4faa1
anjalijain <anjalikosi02@gmail.com>
2023-08-05 14:33:12 +0530

    first commit

---
+++ OwnResume-main/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJkFJUBXtgqSyh__k-pY-8-tABQUbfqV8",
+  authDomain: "ownresumebuilder.firebaseapp.com",
+  databaseURL: "https://ownresumebuilder-default-rtdb.firebaseio.com",
+  projectId: "ownresumebuilder",
+  storageBucket: "ownresumebuilder.appspot.com",
+  messagingSenderId: "792148806617",
+  appId: "1:792148806617:web:2879fa0e34c5fbd18da7c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
